370ead921b9df4f0c45333ebdcd8c9a7a9ee7124
her-cat <i@her-cat.com>
2021-07-23 17:59:32 +0800

    feat: Configuration management

---
+++ global/setting.go
@@ -0,0 +1,11 @@
+package global
+
+import (
+	"github.com/her-cat/golb/pkg/setting"
+)
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
